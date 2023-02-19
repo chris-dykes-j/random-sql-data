c269fecc552791896084aaaceb9a4043fac74f86
Chris Dykes <chris.t.c.dykes@gmail.com>
2023-02-19 11:26:43 -0500

    Add more methods for FigureBuilder

--- RandomSqlData/FigureBuilder.cs
+++ RandomSqlData/FigureBuilder.cs
@@ -1,51 +1,77 @@
+using System.Globalization;
+
 namespace RandomSqlData;
 
 public class FigureBuilder
 {
     private readonly Random _random;
+    private readonly string[] _nameOptions;
     private readonly List<string> _characterOptions;
     private readonly List<string> _brandOptions;
+    private readonly List<string> _seriesOptions;
 
     public FigureBuilder()
     {
         _random = new Random();
-        _characterOptions = new List<string> { "Miku", "Snow Miku", "Cherry Miku", "Ryza" };
+        _nameOptions = File.ReadAllLines("/home/chris/RiderProjects/RandomSqlData/english-adjectives.txt");
+        _characterOptions = new List<string> { "Miku", "Rem", "Sonico", "Ryza" };
         _brandOptions = new List<string> { "Good Smile", "Alter", "Broccoli" };
+        _seriesOptions = CreateRandomSeriesOptions();
     }
 
     public FigureModel CreateRandomFigure()
     {
+        var character = SelectRandomCharacter();
         return new FigureModel
         {
-            Name = CreateRandomString(_random.Next(0, 30)),
-            Character = SelectRandomCharacter(),
+            Name = CreateRandomName(character),
+            Character = character,
             Brand = SelectRandomBrand(), /*
             ReleaseDate = CreateRandomDate(),
+            JanCode = CreateUniqueJanCode(), */
+            Series = ChooseRandomSeries(),
             ReleasePrice = CreateRandomPrice(),
-            JanCode = CreateUniqueJanCode(),
-            Series = CreateRandomSeries(),
-            ProductLine = CreateRandomProductLine(),
-            Sculptor = CreateRandomSculptor() */
+            ProductLine = CreateRandomString(_random.Next(0, 20)),
+            Sculptor = CreateRandomString(_random.Next(0, 15))
         };
+    }
+
+    private string ChooseRandomSeries() => _seriesOptions[_random.Next(0, _seriesOptions.Count - 1)];
+
+    private List<string> CreateRandomSeriesOptions()
+    {
+        var options = new List<string>();
+        using (var reader = new StreamReader("/home/chris/RiderProjects/RandomSqlData/anime.csv"))
+        {
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line!.Split(",");
+                options.Add(values[1]);
+            }
+        }
+        return options;
+    }
+
+    private int CreateRandomPrice() => _random.Next(10_000, 50_000);
+
+    private string CreateRandomName(string character)
+    {
+        var name = _nameOptions[_random.Next(0, _nameOptions.Length - 1)];
+        return char.ToUpper(name[0]) + name[1..] + " " + character;
     }
 
     private string CreateRandomString(int stringLength)
     {
-        var result = "";
-        for (var i = 0; i < stringLength; i++)
+        var result = Convert.ToChar(_random.Next(0, 26) + 65).ToString();
+        for (var i = 0; i < stringLength - 1; i++)
         {
-            result += Convert.ToChar(_random.Next(0, 26) + 65);
+            result += Convert.ToChar(_random.Next(0, 26) + 65 + 6 + 26);
         }
         return result;
     }
 
-    private string SelectRandomCharacter()
-    {
-        return _characterOptions[_random.Next(0, _characterOptions.Count - 1)];
-    }
+    private string SelectRandomCharacter() => _characterOptions[_random.Next(0, _characterOptions.Count - 1)];
 
-    private string SelectRandomBrand()
-    {
-        return _brandOptions[_random.Next(0, _brandOptions.Count - 1)];
-    }
+    private string SelectRandomBrand() => _brandOptions[_random.Next(0, _brandOptions.Count - 1)];
 }
--- RandomSqlData/Program.cs
+++ RandomSqlData/Program.cs
@@ -7,7 +7,11 @@ for (var i = 0; i < 50; i++)
 {
     script += AppendToScript(figureBuilder.CreateRandomFigure());
 }
-string AppendToScript(FigureModel figure) => $"{figure.Name}, {figure.Character}, etc...\n";
+
+string AppendToScript(FigureModel figure)
+{
+    return $"{figure.Name}, {figure.Character}, {figure.Brand}, {figure.Series}, {figure.ProductLine}, {figure.Sculptor}\n";
+}
 
 await File.WriteAllTextAsync("/home/chris/RiderProjects/RandomSqlData/random-figure.sql", script);
 
